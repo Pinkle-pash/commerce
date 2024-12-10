aa8e5ec5717a747c9c660f58a30a8b12d27a3af7
Pinkle-pash <pashpinklepirate@gmail.com>
2024-12-09 17:14:33 -0800

    Improves form submissions and updates dependencies

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -7,7 +7,7 @@ export async function Carousel() {
   const products = await getCollectionProducts({ collection: 'hidden-homepage-carousel' });
 
   if (!products?.length) return null;
-
+  // Collections that start with `hidden-*` are hidden from the search page.
   // Purposefully duplicating products to make the carousel loop and not run out of products on wide screens.
   const carouselProducts = [...products, ...products, ...products];
 
